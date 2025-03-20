ef37b8ffb41027ca5b6dfd3941c61bc5ad468162
jesusramos13 <jesus.130393@gmail.com>
2025-03-21 00:56:04 +0100

    backend

---
+++ src/main/java/com/mi_web/app/MiWebTfgApplication.java
@@ -0,0 +1,13 @@
+package com.mi_web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiWebTfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiWebTfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mi_web/app/MiWebTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi_web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiWebTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
