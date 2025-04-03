af138146d186ed2a687aeed77dee5236f1add608
jesusramos13 <jesus.130393@gmail.com>
2025-04-03 23:20:55 +0200

    version

---
+++ src/app/code-qr/code-qr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-qr',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-qr.component.html',
+  styleUrl: './code-qr.component.css'
+})
+export class CodeQRComponent {
+
+}
