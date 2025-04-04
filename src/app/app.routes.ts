700b5314cb3701b5ab9e2a7b5a3dbecfac65dcf0
arggrm <arramme.96@gmail.com>
2025-04-04 15:29:14 +0200

    feat: add initial components for login, profile, checkout, register, not-found, dashboard, and kitchen-display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'user', component: ProfileComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'app', component: DashboardComponent },
+    { path: '**', component: NotFoundComponent },
   ];
---
+++ src/app/seller/kitchen-display/kitchen-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './kitchen-display.component.html',
+  styleUrl: './kitchen-display.component.css'
+})
+export class KitchenDisplayComponent {
+
+}
